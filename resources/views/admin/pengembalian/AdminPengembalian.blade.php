d16bfd8b8e1214a8cd6b92e3f57b550a904dec97
AntolKun <aloysiusluilui@gmail.com>
2024-12-02 00:08:22 +0700

    max

--- resources/views/admin/pengembalian/AdminPengembalian.blade.php
+++ resources/views/admin/pengembalian/AdminPengembalian.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.AdminTemplate')
 
+@section('css')
+<link href="{{ asset('skoteassets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="{{ asset('skoteassets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('skoteassets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ asset('skoteassets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('skoteassets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('skoteassets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+@stop
+
 @section('content')
 <div class="row">
   <div class="col-12">
@@ -64,4 +73,47 @@
     </table>
   </div>
 </div>
+@section('js')
+<!-- Required datatable js -->
+<script src="{{ asset('skoteassets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<!-- Buttons examples -->
+<script src="{{ asset('skoteassets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+
+<!-- Responsive examples -->
+<script src="{{ asset('skoteassets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('skoteassets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+
+<!-- Datatable init js -->
+<script src="{{ asset('skoteassets/js/pages/datatables.init.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@if ($message = session()->get('success'))
+<script type="text/javascript">
+  Swal.fire({
+    icon: 'success',
+    title: 'Sukses!',
+    text: '{{ $message }}',
+  })
+</script>
+@endif
+
+@if ($message = session()->get('error'))
+<script type="text/javascript">
+  Swal.fire({
+    icon: 'error',
+    title: 'Waduh!',
+    text: '{{ $message }}',
+  })
+</script>
+@endif
+
+@stop
+
 @endsection
